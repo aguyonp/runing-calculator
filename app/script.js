bb1e1096761b949b0dde5997f893b8b2534ef76c
Antoine Guyon <f.guyonantoine@gmail.com>
2023-01-09 15:02:38 +0000

    testing

--- app/script.js
+++ app/script.js
@@ -1,3 +1,20 @@
+function speedToPace(speed) {
+  // Calculer le temps en secondes par km
+  var secondsPerKm = 3600 / speed;
+
+  // Convertir le temps en minutes et secondes
+  var minutes = Math.floor(secondsPerKm / 60);
+  var seconds = Math.round(secondsPerKm % 60);
+
+  // Ajouter un zéro devant les secondes si nécessaire
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  // Retourner l'allure sous forme de chaîne de caractères
+  return minutes + ":" + seconds + "/km";
+}
+
 function calculatePerformance() {
   // Récupère la distance parcourue et le temps effectué
   const distance = Number(document.getElementById("distance").value);
@@ -12,13 +29,13 @@ function calculatePerformance() {
   const speedKMH = distance / 1000 / (timeMinutesTotal / 60);
 
   // Calcule la vitesse en minutes par KM
-  const speedMinPerKM = timeMinutesTotal / (distance / 1000);
+  const speedMinPerKM = speedToPace(speedKMH);
 
   // Arrondit les résultats de vitesse au dixième
   const roundedSpeedKMH = speedKMH.toFixed(1);
-  const roundedSpeedMinPerKM = speedMinPerKM.toFixed(2);
+  // const roundedSpeedMinPerKM = speedMinPerKM.toFixed(2);
 
   // Affiche les résultats dans la page HTML
   document.getElementById("speedKMH").innerHTML = roundedSpeedKMH;
-  document.getElementById("speedMinPerKM").innerHTML = roundedSpeedMinPerKM;
+  document.getElementById("speedMinPerKM").innerHTML = speedMinPerKM;
 }
