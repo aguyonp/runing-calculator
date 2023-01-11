d8f36eeb13f95e8c438726a3b0e38f109f2338e8
Antoine Guyon <f.guyonantoine@gmail.com>
2023-01-11 11:07:01 +0000

    add function

--- app/script.js
+++ app/script.js
@@ -32,10 +32,38 @@ function calculatePerformance() {
   const speedMinPerKM = speedToPace(speedKMH);
 
   // Arrondit les résultats de vitesse au dixième
-  const roundedSpeedKMH = speedKMH.toFixed(1);
-  // const roundedSpeedMinPerKM = speedMinPerKM.toFixed(2);
+  const roundedSpeedKMH = speedKMH.toFixed(2);
 
   // Affiche les résultats dans la page HTML
   document.getElementById("speedKMH").innerHTML = roundedSpeedKMH + ' km/h';
   document.getElementById("speedMinPerKM").innerHTML = speedMinPerKM;
+}
+
+function calculatePassageTime() {
+  // Récupère la distance à laquelle calculer le temps de passage
+  const passageKm = Number(document.getElementById("passageKm").value);
+  
+  // Récupère la distance parcourue et le temps effectué
+  const distance = Number(document.getElementById("distance").value);
+  const timeHours = Number(document.getElementById("hours").value);
+  const timeMinutes = Number(document.getElementById("minutes").value);
+  const timeSeconds = Number(document.getElementById("seconds").value);
+  
+  // Convertit le temps en minutes
+  const timeMinutesTotal = timeHours * 60 + timeMinutes + timeSeconds / 60;
+  
+  // Calcule la vitesse en KM/H
+  const speedKMH = distance / 1000 / (timeMinutesTotal / 60);
+  
+  // Calcule le temps de passage en secondes
+  const passageTimeSeconds = passageKm / speedKMH * 3600;
+  
+  // Calcule le temps de passage en heures, minutes et secondes
+  const passageTimeHours = Math.floor(passageTimeSeconds / 3600);
+  const passageTimeMinutes = Math.floor((passageTimeSeconds % 3600) / 60);
+  const passageTimeSecondsFormated = Math.floor(passageTimeSeconds % 60);
+  const passageTimeMinutesFormated = passageTimeMinutes < 10 ? '0'+passageTimeMinutes : passageTimeMinutes;
+  
+  // Affiche le temps de passage dans le span passageTime
+  document.getElementById("passageTime").innerHTML = passageTimeHours + ":" + passageTimeMinutesFormated + ":" + passageTimeSecondsFormated;
 }
